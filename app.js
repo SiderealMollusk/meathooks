619aa229f1f2d9132cbb03252ada863ac7c666a5
Virvil <77857751+SiderealMollusk@users.noreply.github.com>
2023-08-24 09:57:36 -0700

    App cleaned up

--- app.js
+++ app.js
@@ -4,14 +4,40 @@ const {Meathooks, MKResult} = require('./meathooks/Meathooks');
 const projectsDir = './projects'; // Replace with your projects directory
 const meathooks = new Meathooks(projectsDir);
 let res;
-res = meathooks.setActiveProject('SaveTheCatApp');
-res = meathooks.loadGenerator('cats');
-let catGen;
-if (res.success) { catGen = res.data; } else { console.log(res.message); return; }
 
-catGen.Generate(2).then(result => {
-    console.log('Cat generation result');
-    console.log(result.data);
-  }).catch(error => {
-    console.error('Cat generation error:', error);
-});
+// Create a new project
+res = meathooks.createProject('HelloEcho');
+console.log(res.message);
+
+// OR Set the active project, ( and comment out the createProject line above )
+//res = meathooks.setActiveProject('HelloEcho');
+console.log(res.message);
+
+// Create a new generator, then comment out the createGenerator line below
+res = meathooks.createGenerator('musicians');
+console.log(res.message);
+
+//MANTUALLY EDIT THE GENERATOR FILE TO ADD STUFF YOU LIKE
+
+//Load a generator from a file to instantiate object and use it
+res = meathooks.loadGenerator('musicians');
+console.log(res.message);
+
+// Generate some stuff
+async function generateMusician() {
+  let res;
+  res = meathooks.setActiveProject('HelloEcho');
+  console.log(res.message);
+  
+  res = meathooks.loadGenerator('musicians');
+  console.log(res.message);
+  const musicianFactory = res.data;
+  
+  res = await musicianFactory.Generate(2); // Use await here
+  console.log(res);
+}
+
+//generateMusician(); // Uncomment this line after filing things in
+
+
+
